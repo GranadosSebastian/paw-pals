df7a578e35bfe6559907e3ac8485bb173d0efb2b
unknown <amott@dev-10.com>
2022-08-22 16:41:23 -0500

    update

--- src/main/java/learn/pawpals/security/SecurityConfig.java
+++ src/main/java/learn/pawpals/security/SecurityConfig.java
@@ -3,40 +3,41 @@ package learn.pawpals.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.SecurityBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     private final JwtConverter jwtConverter;
 
     public SecurityConfig(JwtConverter jwtConverter) {
         this.jwtConverter = jwtConverter;
     }
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.headers().frameOptions().sameOrigin();
-        return http.build();
-    }
+        http.csrf().disable();
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
+        http.cors();
+
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/authenticate", "/register").permitAll()
+                .antMatchers(HttpMethod.POST, "/refresh-token").authenticated()
+                .antMatchers(HttpMethod.GET, "/api/animal", "/api/animal/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/animal").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/animal/*").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/animal/*").hasAnyRole("ADMIN")
+                .antMatchers("/**").denyAll()
+                .and()
+                .addFilter(new JwtRequestFilter(authenticationManager(), jwtConverter))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
@@ -44,32 +45,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Override
     @Bean
     public AuthenticationManager authenticationManager() throws Exception {
-        return authenticationManager();
+        return super.authenticationManager();
     }
-
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-
-        http.cors();
-
-        http.authorizeRequests()
-                .antMatchers("/authenticate").permitAll()
-                .antMatchers(HttpMethod.GET,
-                        "/order").permitAll()
-                .antMatchers(HttpMethod.GET,
-                        "/sighting", "/sighting/*").permitAll()
-                .antMatchers(HttpMethod.POST,
-                        "/sighting/*").hasAnyRole("USER", "ADMIN")
-                .antMatchers(HttpMethod.PUT,
-                        "/sighting/*").hasAnyRole("USER", "ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/sighting/*").hasRole("ADMIN")
-                .antMatchers("/**").denyAll()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
-
-
 }
