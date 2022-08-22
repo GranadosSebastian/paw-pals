b2ff85b089e3b01f1479b95c1903fc23b3495acc
GranadosSebastian <GranadosSebastian>
2022-08-22 08:07:42 -0600

    commit for morning session

--- client/paw-pals-react-client/src/components/Home.js
+++ client/paw-pals-react-client/src/components/Home.js
@@ -1,9 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+
 
 function Home() {
+
+    const navigate = useNavigate();
+    const navigateToAdopt = () => {
+        navigate('/animals');
+    }
+
+
     return (
         <>
             <h2>Home</h2>
             <p>welcome page with Adopt button and a small list of available pets</p>
+            <button type='button' onClick={navigateToAdopt}>Adopt</button>
         </>
     );
 }
