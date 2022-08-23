f3a97c2827aa5e5ab3423e1c9f6ae4ff3c4d82f7
Keri Salanik <ksalanik12@gmail.com>
2022-08-23 09:04:57 -0500

    morning push

--- src/test/java/learn/pawpals/data/AnimalJdbcRepositoryTest.java
+++ src/test/java/learn/pawpals/data/AnimalJdbcRepositoryTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-class AnimalJdbcRepositoryTest {
+public class AnimalJdbcRepositoryTest {
 
     @Autowired
     AnimalJdbcTemplateRepository repository;
