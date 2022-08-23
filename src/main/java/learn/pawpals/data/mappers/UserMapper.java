c9757575f745c48da4349da64495f0e90c3a1d5b
unknown <amott@dev-10.com>
2022-08-23 11:39:17 -0500

    update

--- src/main/java/learn/pawpals/data/mappers/UserMapper.java
+++ src/main/java/learn/pawpals/data/mappers/UserMapper.java
@@ -9,6 +9,14 @@ import java.sql.SQLException;
 public class UserMapper implements RowMapper<User> {
     @Override
     public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return null;
+        User user = new User();
+        user.setUserId(rs.getInt("user_id"));
+        user.setFirstName(rs.getString("first_name"));
+        user.setLastName(rs.getString("last_name"));
+        user.setAddress(rs.getString("address"));
+        user.setPhone(rs.getString("phone"));
+        user.setEmail(rs.getString("email"));
+        user.setRoleId(rs.getInt("role_id"));
+        return user;
     }
 }
