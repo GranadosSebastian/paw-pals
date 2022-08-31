ca5eb9e75879d475fe4b950b640dadba6d94bb8f
unknown <amott@dev-10.com>
2022-08-31 14:33:21 -0500

    update

---
+++ client/paw-pals-react-client/src/components/Slider.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react'
+import './Slider.css'
+import SliderBtn from './SliderBtn.js'
+import SliderData from './SliderData.js'
+
+export default function Slider() {
+
+  const [slideIndex, setSlideIndex] = useState(1)
+
+  const nextSlide = () => {
+      if(slideIndex !== SliderData.length) {
+        setSlideIndex(slideIndex + 1)
+      } else if (slideIndex === SliderData.length) {
+        setSlideIndex(1)
+      }
+  }
+
+  const prevSlide = () => {
+      if(slideIndex !== 1) {
+        setSlideIndex(slideIndex - 1)
+      } else if (slideIndex === 1) {
+        setSlideIndex(SliderData.length)
+      }
+  }
+
+  const moveDot = index => {
+    setSlideIndex(index)
+  }
+
+  console.log(Array.from)
+  return (
+    <div className="container-slider">
+        {SliderData.map((obj, index) => {
+          return (
+            <div key={obj.id} 
+            className={slideIndex === index + 1 ? "slide active-anim" : "slide"}>
+              
+            <img 
+              src={'public/imgs/img${index + 1}.jpg'}
+              />
+              <p>{obj.title}</p>
+
+              </div>
+          )
+        })}
+        <SliderBtn moveSlide={nextSlide} direction={"next"} />
+        <SliderBtn moveSlide={nextSlide} direction={"prev"} />
+
+        <div className="container-dots">
+            {Array.from({length: 5}).map((item, index) => (
+                <div 
+                onClick={() => moveDot(index + 1)}
+                className={slideIndex === index + 1 ? "dot active" : "dot"}
+                ></div>
+            ))}
+        </div>
+    </div>
+  )
+}
